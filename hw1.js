199ae2ad01af85fe74f254201d85d0f854cb5cca
tongocthach2002 <tongocthach2002@gmail.com>
2020-12-19 15:59:27 +0700

    nop bai tap ve nha buoi 1

---
+++ hw1.js
@@ -0,0 +1,22 @@
+console.log('1.1. Three examples of invalid variable names could be listed as null, 18yrs and boykute@hotvjppro')
+console.log('1.2. In JS, we could use the typeof operator to check a variable data types')
+console.log('2. 1.b, 2.a, 3.e, 4.c ')
+let message='Coding is great'
+console.log(message)
+let studentCount=0
+message='Coding might not be easy, but still great'
+console.log(message)
+studentCount=19
+console.log(studentCount)
+console.log(message.toLowerCase())
+console.log(studentCount+1)
+alert('You look great')
+let Name=prompt('Whats your name?')
+alert(`Hi, ${Name}`)
+let FirstName =prompt('Enter your first name')
+let LastName =prompt('Enter your last name')
+alert(`Hi, ${FirstName+LastName}`)
+let SideLength=prompt('Enter side length of the square')
+alert(`The square area is ${SideLength*SideLength}`)
+let CelciusDegree= prompt('Enter the temprature in Celsius')
+alert(`${CelsiusDegree=CelciusDegree*1.8+32}`)
